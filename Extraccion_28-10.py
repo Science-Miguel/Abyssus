28c2571e4257f2b21dc92b8365f7b253c7d6da6a
Daniela Fernández Salinas <danielaf@lcg.unam.mx>
2019-10-30 15:41:38 -0600

    remove

--- Extraccion_28-10.py
+++ Extraccion_28-10.py
@@ -1,5 +1,6 @@
 import re
 import argparse
+import os
 ####################################################################
 ##### lee argumento: archivo de entrada en linea de comandos    ####
 ####################################################################
@@ -13,21 +14,21 @@ parseoArgs.add_argument(
 		"-f", "--file",
 			help="Archivo de entrada en formato texto", required=True)
 
-
+parseoArgs.add_argument("--outputPath", dest="outputPath", help="Output path to place output files", metavar="PATH")
 my_args = parseoArgs.parse_args()
 
 
-####################################################################
+###################################################################
 ##### Apertura y lectura de archivos, con with                 ####
 ####################################################################
 
+Medio, Bacteria = {},{}
 
 with open(my_args.file, 'r') as file:
-    for line in file:
-        line = re.sub(',',', ', line)
-        match = re.findall(r'\,([^\n|\r|\,]+)', line)
-        if match:
-		Medio[match[0]] = match[4:]
-		Bacteria[match[2]] = match[4:]
-print(Medio)
-print(Bacteria)
+	for line in file:
+		line = re.sub(',',', ', line)
+		match = re.findall(r'\,([^\n|\r|\,]+)', line)
+		if match:
+			Medio[match[0]] = match[4:]
+			Bacteria[match[2]]= match[4:]
+print(Medio,Bacteria)
